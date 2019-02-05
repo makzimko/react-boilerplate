72661ce6cf687b438dab306b3e7df3794ea6a6d3
Maksym Tkachuk <uakorn@gmail.com>
2019-02-06 00:10:47 +0200

    Added code quality tools

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
-const App = () =>
-  (<div>Hello world!</div>);
+const App = () => <div>Hello world!</div>;
 
 export default hot(App);
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {AppContainer} from "react-hot-loader";
+import { AppContainer } from 'react-hot-loader';
 
 import App from './app';
 
-const renderApp = (Component) => render(
-  <AppContainer>
-    <Component/>
-  </AppContainer>,
-  document.querySelector('#root'),
-);
+const renderApp = (Component) =>
+  render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.querySelector('#root'),
+  );
 
 renderApp(App);
 
