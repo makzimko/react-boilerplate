7d313095394ae1193e25e20bbaa903dd1665816a
Maksym Tkachuk <uakorn@gmail.com>
2019-02-06 16:05:41 +0200

    Added redux store and empty root reducer

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
 
 import App from './app';
+import store from './store';
 
 const renderApp = (Component) =>
   render(
     <AppContainer>
-      <Component />
+      <Provider store={store}>
+        <Component />
+      </Provider>
     </AppContainer>,
     document.querySelector('#root'),
   );
